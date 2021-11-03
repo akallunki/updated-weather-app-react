78053c4078e061a4bb48c59f333ae349f929958e
akallunki <88041184+akallunki@users.noreply.github.com>
2021-11-03 20:50:05 +0000

    Updated stylings for weather forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,36 +1,57 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
   return (
-    <div className="row">
+    <div className="row forecast-row">
       <div className="col-3">
-        Mon
-        <div>
-          <span>8</span> | <span>11</span> °C
-        </div>
-        <WeatherIcon code="01d" size={50} />
+        <span className="weather-forecast-text">
+          <div className="weather-weekday">Mon</div>
+          <div className="weather-digits">
+            <span className="weather-max">11°</span> |{" "}
+            <span className="weather-min">8°</span>
+          </div>
+        </span>
+        <span className="icon">
+          <WeatherIcon code="01d" size={50} />
+        </span>
       </div>
       <div className="col-3">
-        Tue
-        <div>
-          <span>4</span> | <span>9</span> °C
-        </div>
-        <WeatherIcon code="01d" size={50} />
+        <span className="weather-forecast-text">
+          <div className="weather-weekday">Tue</div>
+          <div className="weather-digits">
+            <span className="weather-max">9°</span> |{" "}
+            <span className="weather-min">4°</span>
+          </div>
+        </span>
+        <span className="icon">
+          <WeatherIcon code="01d" size={50} />
+        </span>
       </div>
       <div className="col-3">
-        Wed
-        <div>
-          <span>5</span> | <span>13</span> °C
-        </div>
-        <WeatherIcon code="01d" size={50} />
+        <span className="weather-forecast-text">
+          <div className="weather-weekday">Wed</div>
+          <div className="weather-digits">
+            <span className="weather-max">13°</span> |{" "}
+            <span className="weather-min">5°</span>
+          </div>
+        </span>
+        <span className="icon">
+          <WeatherIcon code="01d" size={50} />
+        </span>
       </div>
       <div className="col-3">
-        Thurs
-        <div>
-          <span>9</span> | <span>12</span> °C
-        </div>
-        <WeatherIcon code="01d" size={50} />
+        <span className="weather-forecast-text">
+          <div className="weather-weekday">Thurs</div>
+          <div className="weather-digits">
+            <span className="weather-max">12°</span> |{" "}
+            <span className="weather-min">9°</span>
+          </div>
+        </span>
+        <span className="icon">
+          <WeatherIcon code="01d" size={50} />
+        </span>
       </div>
     </div>
   );
