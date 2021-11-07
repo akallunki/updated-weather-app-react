8f6c44aa7c6e7863116672cbf97a77655fe5dd82
akallunki <88041184+akallunki@users.noreply.github.com>
2021-11-07 17:14:19 +0000

    Forecasts for all days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,9 +16,15 @@ export default function WeatherForecast(props) {
     console.log(forecast);
     return (
       <div className="row forecast-row">
-        <div className="col">
-          <WeatherForecastDay data={forecast[0]} />
-        </div>
+        {forecast.map(function (dailyForecast, index) {
+          if (index < 4) {
+            return (
+              <div className="col" key={index}>
+                <WeatherForecastDay data={dailyForecast} />
+              </div>
+            );
+          }
+        })}
       </div>
     );
   } else {
